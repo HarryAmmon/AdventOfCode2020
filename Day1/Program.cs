37043b009e4c838115d177b9cd84a018e154f873
HarryAmmon <harry_a99@hotmail.co.uk>
2020-12-30 16:28:08 +0000

    Completed Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-
+using System.IO;
 namespace Day1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] fileContent = File.ReadAllLines("input.txt");
+
+
+
+            Console.WriteLine(Part1(fileContent));
+            Console.WriteLine(Part2(fileContent));
+        }
+
+        static int Part1(string[] fileContent)
+        {
+            for (int i = 0; i < fileContent.Length; i++)
+            {
+                for (int j = 1; j < fileContent.Length; j++)
+                {
+                    int result = int.Parse(fileContent[i]) + int.Parse(fileContent[j]);
+                    if (result == 2020)
+                    {
+                        return int.Parse(fileContent[i]) * int.Parse(fileContent[j]);
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        static int Part2(string[] fileContent)
+        {
+            for (int i = 0; i < fileContent.Length; i++)
+            {
+                for (int j = 1; j < fileContent.Length; j++)
+                {
+                    for (int k = 2; k < fileContent.Length; k++)
+                    {
+                        int result = int.Parse(fileContent[i]) + int.Parse(fileContent[j]) + int.Parse(fileContent[k]);
+                        if (result == 2020)
+                        {
+                            return int.Parse(fileContent[i]) * int.Parse(fileContent[j]) * int.Parse(fileContent[k]);
+                        }
+                    }
+                }
+            }
+
+            return -1;
         }
     }
 }
