dc614d1d41b78343f0489810cfb0939cdb076016
HarryAmmon <harry_a99@hotmail.co.uk>
2021-01-02 17:40:09 +0000

    Completed Day2Part2

--- Day2/PasswordValidator.cs
+++ Day2/PasswordValidator.cs
@@ -2,9 +2,9 @@ namespace Day2
 {
     public class PasswordValidator
     {
-        public bool ValidatePassword(Password password)
+        public bool OccurrencePolicy(Password password)
         {
-            int occurence = PolicyOccurence(password.Contain, password.Pword);
+            int occurence = CountOccurrence(password.Contain, password.Pword);
             if (occurence >= password.LowerBound && occurence <= password.UpperBound)
             {
                 return true;
@@ -12,7 +12,7 @@ namespace Day2
             else return false;
         }
 
-        private int PolicyOccurence(char policy, string pword)
+        private int CountOccurrence(char policy, string pword)
         {
             char[] pwordAsChars = pword.ToCharArray();
             int count = 0;
@@ -24,6 +24,18 @@ namespace Day2
                 }
             }
             return count;
+        }
+
+        public bool PositionPolicy(Password password)
+        {
+            char[] pwordAsChars = password.Pword.ToCharArray();
+            int firstPosition = password.LowerBound - 1;
+            int secondPosition = password.UpperBound - 1;
+            if (pwordAsChars[firstPosition] == password.Contain ^ pwordAsChars[secondPosition] == password.Contain) // XOR
+            {
+                return true;
+            }
+            else return false;
         }
     }
 }
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,32 +6,50 @@ namespace Day2
 {
     class Program
     {
+        static PasswordValidator validator = new PasswordValidator();
         static void Main(string[] args)
         {
             string[] fileLines = File.ReadAllLines("input.txt");
 
-            Console.WriteLine("Hello World!");
-
             Console.WriteLine(Part1(fileLines));
+            Console.WriteLine(Part2(fileLines));
         }
 
 
         static int Part1(string[] fileLines)
         {
             List<Password> pwordList = new List<Password>();
-            PasswordValidator validator = new PasswordValidator();
             int validPasswords = 0;
 
             foreach (string line in fileLines)
             {
                 Password password = new Password(line);
-                password.Valid = validator.ValidatePassword(password);
+                password.Valid = validator.OccurrencePolicy(password);
                 pwordList.Add(password);
                 if (password.Valid)
                 {
                     validPasswords++;
                 }
 
+            }
+
+            return validPasswords;
+        }
+
+        static int Part2(string[] fileLines)
+        {
+            List<Password> pwordList = new List<Password>();
+            int validPasswords = 0;
+
+            foreach (string line in fileLines)
+            {
+                Password password = new Password(line);
+                password.Valid = validator.PositionPolicy(password);
+                pwordList.Add(password);
+                if (password.Valid)
+                {
+                    validPasswords++;
+                }
             }
 
             return validPasswords;
